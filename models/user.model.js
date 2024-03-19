1e18bfee5621e1bd7c42987249c2c6af99d97f8b
trilvid <trilztech34@gmail.com>
2024-03-19 06:32:34 -0700

    fixed main bug

--- models/user.model.js
+++ models/user.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const user = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
-    lastname: { type: String, required: true,unique: true },
+    lastname: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
